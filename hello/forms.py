83b28485c8b483ce19f6b5ee728a5f4a3f4557e0
SaiGopalaKrishnaMaya <saigopalakrishnamaya@gmail.com>
2025-01-01 15:08:42 +0530

    Delete hello directory

--- hello/forms.py
+++
@@ -1,8 +0,0 @@
-# hello/forms.py
-
-from django import forms
-
-class HousePriceForm(forms.Form):
-    beds = forms.IntegerField(label='Beds', min_value=1)
-    baths = forms.FloatField(label='Baths', min_value=1.0)
-    size = forms.IntegerField(label='Size (sqft)', min_value=100)
--- hello/urls.py
+++
@@ -1,8 +0,0 @@
-# hello/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Make sure this points to the index view
-]
